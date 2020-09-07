9aea7c994db83c1056c863f529a27dfb6414f83d
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-07 22:39:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
-/* Your Code Here */
-let createEmployeeRecord = function(array) {
-  //array = [string, string, string, number]
-  let employeeObject = {
-    firstName: array[0], 
-    familyName: array[1], 
-    title: array[2], 
-    payPerHour: array[3],
-    timeInEvents: [], 
-    timeOutEvents: []
-  }
-  return employeeObject; 
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
 }
 
-let createEmployeeRecords = function(arrayOfArrays) {
-  //arrayOfArrays = [[string, string, string, number], [string, string, string, number], ...]
-  let newArray = arrayOfArrays.map(array => createEmployeeRecord(array))
-  return newArray;
+let createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
 }
 
-let createTimeInEvent = function(dateStamp) {
-  //A date stamp ("YYYY-MM-DD HHMM")
-  let [date, hour] = dateStamp.split(" ")
-  this.timeInEvents.push({
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
         type: "TimeIn",
         hour: parseInt(hour, 10),
         date,
     })
 
-  return this
+    return this
 }
 
-let createTimeOutEvent = function(dateStamp) {
-  //A date stamp ("YYYY-MM-DD HHMM")
-  let [date, hour] = dateStamp.split(" ")
-  this.timeOutEvents.push({
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
         type: "TimeOut",
         hour: parseInt(hour, 10),
         date,
     })
 
-  return this
+    return this
 }
 
 let hoursWorkedOnDate = function(soughtDate){
@@ -82,47 +79,4 @@ let calculatePayroll = function(arrayOfEmployeeRecords){
     return arrayOfEmployeeRecords.reduce(function(memo, rec){
         return memo + allWagesFor.call(rec)
     }, 0)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
 }
